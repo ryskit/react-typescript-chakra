2be149dcfa98804f37dc5e2b862d74a529c68af2
ryskit <ryosukekida.71@gmail.com>
2022-10-25 03:06:37 +0900

    :sparkles: ヘッダー作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
-import "./App.css";
 import theme from "./theme/theme";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router/Router";
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,19 +1,41 @@
 import { memo } from "react";
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "../components/pages/Home";
 import { Login } from "../components/pages/Login";
 import { Page404 } from "../components/pages/Page404";
 import { Setting } from "../components/pages/Setting";
 import { UserManagement } from "../components/pages/UserManagement";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 
 export const Router = memo((props: any) => {
   return (
     <Routes>
       <Route path="/" element={<Login />}></Route>
       <Route path="/home">
-        <Route index element={<Home />} />
-        <Route path="user_measurement" element={<UserManagement />} />
-        <Route path="setting" element={<Setting />} />
+        <Route
+          index
+          element={
+            <HeaderLayout>
+              <Home />
+            </HeaderLayout>
+          }
+        />
+        <Route
+          path="user_measurement"
+          element={
+            <HeaderLayout>
+              <UserManagement />
+            </HeaderLayout>
+          }
+        />
+        <Route
+          path="setting"
+          element={
+            <HeaderLayout>
+              <Setting />
+            </HeaderLayout>
+          }
+        />
       </Route>
       <Route path="*" element={<Page404 />} />
     </Routes>
