470b5b0ba2309f4fa6cc7c9ab40a2ecbe1cfb5c2
ryskit <ryosukekida.71@gmail.com>
2022-10-27 01:49:31 +0900

    :sparkles: ログイン画面の作成

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,29 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
 import { memo } from "react";
 
 export const Login = memo((props: any) => {
-  return <p>Login</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          UserManagement
+        </Heading>
+        <Divider my="4" />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="User ID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+            Login
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
