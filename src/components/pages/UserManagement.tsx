39efe92ad3d5af221e260f8f62f60409c6248b65
ryskit <ryosukekida.71@gmail.com>
2022-10-30 02:32:14 +0900

    :sparkles: ユーザーの一覧取得実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,32 @@
-import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
-import { memo } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/userCard";
 
 export const UserManagement = memo((props: any) => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="ryskit"
-          fullName="Ryosuke Kida"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} spacing="12" justify="center">
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
