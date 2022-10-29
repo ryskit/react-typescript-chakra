829ff38450eb4b1d46eadc66a2e5df28fd457168
ryskit <ryosukekida.71@gmail.com>
2022-10-30 02:02:40 +0900

    :sparkles: ユーザーカードコンポーネントの作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,17 @@
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo } from "react";
+import { UserCard } from "../organisms/user/userCard";
 
 export const UserManagement = memo((props: any) => {
-  return <p>UserManagement</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="ryskit"
+          fullName="Ryosuke Kida"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
